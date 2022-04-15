3a7fc3f714e10e47f98c46557c2bb5689320a958
Pamela Dayne <pam.dayne@gmail.com>
2022-04-15 18:27:32 +0800

    Render list and display data

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const db_local = 'stays.json'
-const db_url = 'https://raw.githubusercontent.com/pamdayne/wind-d-bnb/main/db.json'
+const db_url = 'https://raw.githubusercontent.com/pamdayne/wind-d-bnb/main/db/db.json'
 
 async function fetchLocations() {
 	try {
@@ -12,11 +12,41 @@ async function fetchLocations() {
 
 async function renderList() {
 	let places = await fetchLocations()
+	let totalPlaces = places.length
 	let html = ''
 
+	console.log(places)
+	console.log(totalPlaces)
+
+
 	places.forEach(place => {
-		html = + `<div>
-					</div>`
+		let superHost = 'Super Host'
+
+		html += `<div class="lists-wrapper">
+								<div class="photo">
+									<img
+										src="${place.photo}"
+										alt="${place.title}" />
+								</div>
+								<div class="details">
+									<div class="unit">
+										<div class="host">${place.superHost ? 'Super Host' : ''}</div>
+										<div class="type">${place.type}</div>
+										<div class="bed">
+											<span class="total">${place.beds}</span>
+											<span class="txt-bed">beds</span>
+										</div>
+										<div class="score">
+											<span class="material-icons">star</span>
+											<span class="rating">${place.rating}</span>
+										</div>
+									</div >
+									<div class="short-desc">${place.title}</div>
+								</div >
+							</div > `
+
+		document.querySelector('#locations').innerHTML = html
+
 	})
 
 }

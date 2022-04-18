c1025053a20787dea2d074271ad338b1dff37758
Yuyu S <swsalim@gmail.com>
2022-04-18 16:18:16 +0800

    code review

--- script.js
+++ script.js
@@ -1,34 +1,49 @@
-const db_local = 'stays.json'
-const db_url = 'https://raw.githubusercontent.com/pamdayne/wind-d-bnb/main/db/stays.json'
+const db_local = "stays.json";
+const db_url =
+  "https://raw.githubusercontent.com/pamdayne/wind-d-bnb/main/db/stays.json";
 
 async function fetchLocations() {
-	try {
-		let resp = await fetch(db_url)
-		return await resp.json()
-	} catch (err) {
-		console.error(err)
-	}
+  try {
+    let resp = await fetch(db_url);
+    return await resp.json();
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 async function renderList() {
-	let places = await fetchLocations()
-	let html = ''
+  let places = await fetchLocations();
+  // IMPROVE: try to destructure the properties
+  // const { photo, title, superHost, type } = await fetchLocations();
 
-	places.forEach(place => {
-		setCountry(place.country)
+  let html = "";
 
-		html += `<div class="lists-wrapper">
+  places.forEach((place) => {
+    // IMPROVE: this will get called every iteration
+    setCountry(place.country);
+
+    // IMPROVE: try to use:
+    // https://developer.mozilla.org/en-US/docs/Web/API/Document/createElement
+    // https://developer.mozilla.org/en-US/docs/Web/API/Element/append
+    // https://developer.mozilla.org/en-US/docs/Web/API/Node/appendChild
+    html += `<div class="lists-wrapper">
 								<div class="photo">
 									<img
-										src="${place.photo}"
-										alt="${place.title}" />
+										src="${places.photo}"
+										alt="${places.title}"
+										loading
+									/>
 								</div>
 								<div class="details">
 									<div class="unit">
-									${place.superHost != false ? '<div class="host"> Super Host</div>' : ''}
+									${places.superHost != false ? '<div class="host"> Super Host</div>' : ""}
 										<div class="type">${place.type}</div >
 										<div class="bed">
-										${place.beds != null ? '<span class="total">' + place.beds + ' beds</span > ' : ''}
+										${
+                      place.beds != null
+                        ? '<span class="total">' + place.beds + " beds</span > "
+                        : ""
+                    }
 										</div >
 										<div class="score">
 										<span class="material-icons">star</span>
@@ -36,14 +51,17 @@ async function renderList() {
 										</div>
 									</div>
 									<div class="short-desc">${place.title}</div>
-								</div >
-							</div > `
-		document.querySelector('#locations').innerHTML = html
-	})
+								</div>
+							</div> `;
+
+    // IMPROVE: #locations.innerHTML will get replaced with the html in every iteration
+    document.querySelector("#locations").innerHTML = html;
+  });
 }
 
 function setCountry(country) {
-	document.querySelector("#country").innerHTML = 'Stays in ' + country
+  document.querySelector("#country").innerHTML = "Stays in " + country;
 }
 
-renderList()
+// IMPROVE: try to use https://developer.mozilla.org/en-US/docs/Web/API/Window/DOMContentLoaded_event
+renderList();
